b68e9429407e0038101f08f11f482782d31b5807
danekshea <danekshea@gmail.com>
2022-04-27 00:55:37 -0600

    updated deploy contract for gas limit

--- src/L1/deploy-contract.ts
+++ src/L1/deploy-contract.ts
@@ -1,8 +1,8 @@
 import yargs from 'yargs';
 import { deployContract } from '../utils/L1Helpers/deploy-contract'
 
-async function main(ownerPrivateKey: string, contract:string, name:string, symbol:string, network:string, gasLimit:string): Promise<void> {
-    const deployedContract = await deployContract(ownerPrivateKey, contract, name, symbol, network, gasLimit);
+async function main(ownerPrivateKey: string, contract:string, name:string, symbol:string, network:string, gasLimit:string, gasPrice:string): Promise<void> {
+    const deployedContract = await deployContract(ownerPrivateKey, contract, name, symbol, network, gasLimit, gasPrice);
     console.log('Deployed contract address:', deployedContract.address)
     console.log('Deployer account:', await deployedContract.signer.getAddress())
 }
@@ -15,11 +15,13 @@ const argv = yargs(process.argv.slice(2))
     c: { describe: 'contract to deploy, must have artifact in /artifacts', type: 'string', demandOption: true },
     n: { describe: 'name of the contract', type: 'string', demandOption: true },
     y: { describe: 'symbol', type: 'string', demandOption: true },
+    g: { describe: 'gas limit', type: 'string', demandOption: true },
+    p: { describe: 'gas price', type: 'string', demandOption: true },
     network: { describe: 'network. ropsten or mainnet', type: 'string', demandOption: true}
   })
   .parseSync();
 
-main(argv.k, argv.c, argv.n, argv.y, argv.network)
+main(argv.k, argv.c, argv.n, argv.y, argv.g, argv.p, argv.network)
   .then(() => console.log('Contract deployment complete'))
   .catch(err => {
     console.error(err);
